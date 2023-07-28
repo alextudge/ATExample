068a8b54a77e327c6f8fafea020ef9e49f8a5b30
Alex Tudge <alextudge@hotmail.co.uk>
2023-07-28 16:26:37 +0100

    Break up UI pyramid

--- Modules/Features/ATUsersKit/Sources/ATUsersKit/Views/ATUsersHomeView.swift
+++ Modules/Features/ATUsersKit/Sources/ATUsersKit/Views/ATUsersHomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ATDataModel
 import ATUIKit
 
 struct ATUsersHomeView: View {
@@ -16,32 +17,43 @@ struct ATUsersHomeView: View {
         ATLoadableView(state: $viewModel.state) {
             ScrollView(.horizontal) {
                 LazyHStack {
-                    ForEach(viewModel.users) { user in
-                        VStack {
-                            ATUserView(viewModel: ATUserViewModel(user: user))
-                            HStack {
-                                Button(String(localized: "Posts")) {
-                                    viewModel.view(.posts, userId: user.id)
-                                }
-                                Button(String(localized: "Albums")) {
-                                    viewModel.view(.albums, userId: user.id)
-                                }
-                            }
-                            ATUserTodoView(viewModel: ATUserTodoViewModel(userId: user.id,
-                                                                          networkService: viewModel.networkService))
-                        }
-                        .containerRelativeFrame(.horizontal)
-                        .edgeFadeAnimation()
-                    }
+                    userCardCarousel()
                 }
                 .scrollTargetLayout()
             }
             .scrollTargetBehavior(.viewAligned)
             .safeAreaPadding(.horizontal)
         }
-        .navigationTitle("Contacts")
+        .navigationTitle(String(localized: "Contacts"))
         .task {
             await viewModel.loadData()
         }
+    }
+}
+
+private extension ATUsersHomeView {
+    func userCardCarousel() -> some View {
+        ForEach(viewModel.users) { user in
+            VStack {
+                ATUserView(viewModel: ATUserViewModel(user: user))
+                buttons(for: user.id)
+                ATUserTodoView(viewModel: ATUserTodoViewModel(userId: user.id,
+                                                              networkService: viewModel.networkService))
+            }
+            .containerRelativeFrame(.horizontal)
+            .edgeFadeAnimation()
+        }
+    }
+    
+    func buttons(for userId: Int) -> some View {
+        HStack {
+            Button(String(localized: "Posts")) {
+                viewModel.view(.posts, userId: userId)
+            }
+            Button(String(localized: "Albums")) {
+                viewModel.view(.albums, userId: userId)
+            }
+        }
+        .buttonStyle(.bordered)
     }
 }
